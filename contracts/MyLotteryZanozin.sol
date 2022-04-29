dc5492440f09fad45bb7cc5d7cd3dd4a6f6f8532
ZaniInc <stridestoreholding@gmail.com>
2022-04-29 16:56:56 +0300

    first commit

---
+++ contracts/MyLotteryZanozin.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Lottery {
+
+    mapping(address => uint)_ticketNumber;
+    mapping(address => uint)_balances;
+
+    address  payable [] private members;
+    address payable private owner = payable(msg.sender) ;
+    uint public numberOfMembers ;
+    
+    modifier onlyOwner () {
+        require (msg.sender == owner);
+        _;
+    }
+
+    // This function we use to check number of lottery ticket
+    function myTicket () public view returns (uint) {
+        return _ticketNumber[msg.sender];
+    }
+
+
+   /**
+    IMPORTANTS THINKS OF THIS FUNCTION IS ADD NEW MEMBER TO ARRAY
+    AND BUY 1 TICKET FOR 1 ETHER
+    */
+    function buyTicket () public payable {
+        require(numberOfMembers <= 10 ether && msg.value == 1 ether && _balances[msg.sender] < 1);
+        numberOfMembers += msg.value / 1000000000000000000;
+        payable(msg.sender).transfer(1);
+        _ticketNumber[msg.sender] += members.length + 1;
+        _balances[msg.sender] += 1;
+
+        // address add to list with members
+        members.push(payable(msg.sender));
+    }
+
+    // This function we use to create random number by creating random hash.
+    function randomNumber () private view returns(uint){
+        address owner_ = msg.sender;
+        return uint (keccak256(abi.encodePacked(owner_ , block.timestamp)));
+    }
+   /**
+    WE USE RESULT OF FUNCTION randomNumber like random number and after % on length of array.
+    After this 90% of balance we transfer to random winner and 10 to owner of contract.
+    And last we reset members of Lottery.
+    Function will start when bank = 10 ether.
+    */
+    function pickWinner () public onlyOwner returns(uint) {
+        require(bankOfLottery() > 1 ether);
+        sendToOwner();
+        uint winner = randomNumber() % members.length;
+        members[winner].transfer(address(this).balance);
+        members = new address payable[](0);
+        numberOfMembers = 0;
+        return winner;
+
+    }
+
+    // CHECK BANK OF LOTTERY
+    function bankOfLottery () public view returns(uint) {
+        return address(this).balance;
+    }
+
+    // CHECK MEMBERS OF LOTTERY
+
+    function membersOfLottery () public view returns (address payable [] memory) {
+        return members;
+    }
+
+    // Send 10% to contract owner
+    function sendToOwner () private {
+       
+        uint a = 10 * bankOfLottery() / 100;
+        owner.transfer(a);
+    }
+}
---
+++ test/lottery.test.js
@@ -0,0 +1,61 @@
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
+
+describe("Lottery" , function () {
+
+let acc1;
+let acc2;
+let lottery;
+
+ before(async function () {
+    
+    [acc1, acc2] = await ethers.getSigners();
+    const Lottery = await ethers.getContractFactory("Lottery" , acc1);
+    lottery = await Lottery.deploy();
+    await lottery.deployed();
+    
+ });
+
+ it("Buy Ticket" , async function () {
+
+    let ticket = await lottery.buyTicket({value : 1000000000000000000n}) ;
+    await ticket.wait();
+  
+   })
+
+ it("Buy Ticket 2" , async function () {
+
+    let ticketForMember = await lottery.connect(acc2).buyTicket({value : 1000000000000000000n}) ;
+    await ticketForMember.wait();
+  
+   })
+
+ it("Check number of ticket + bank of lottery + members of lottery" , async function () {
+
+    
+    let ticketMy = await lottery.myTicket() ;
+    expect(ticketMy).to.eq(1);
+    console.log(ticketMy);
+
+    let bankOfLottery = await lottery.bankOfLottery();
+    // expect(bankOfLottery).to.equal(1000000000000000000n);
+    console.log(bankOfLottery);
+
+    let membersOfLottery = await lottery.membersOfLottery();
+    console.log(membersOfLottery);
+  
+ })
+
+ it("Pick Winner" , async function () {
+
+  let winner = await lottery.pickWinner() ;
+  console.log(acc1.address);
+  console.log(acc2.address);
+  console.log(winner.value);
+  
+
+ })
+
+
+
+})
